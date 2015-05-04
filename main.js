ff0d172f9654cbbddea0056710651f500c0d7869
Leon Coto <leoncoto@gmail.com>
2015-05-04 09:22:17 +0100

    Basic webgl setup

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 (function(){
   "use strict";
-  console.log('ok')
+
+  var gl = document.getElementById('glcanvas').getContext('webgl')
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
+  gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT)
+
 })()
